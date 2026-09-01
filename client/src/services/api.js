2c713a2a26cfd74f147c92083aaf248808e63bcc
Partha Pattanayak <parthapattanayak2002@gmail.com>
2026-09-01 10:59:31 +0530

    fix api error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "hhttps://team-task-manager-backend-6dra.onrender.com/api",
+  baseURL: "https://team-task-manager-backend-6dra.onrender.com/api",
   // baseURL: "http://localhost:5000/api",
 });
 
